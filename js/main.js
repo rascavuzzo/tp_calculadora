1e9a3e170c04bda086926b6c63e5a84fb3e18331
Raul Scavuzzo <raul.scavuzzo@itscordoba.edu.ar>
2024-06-07 20:16:28 -0300

    correcciones y ecritura en pantalla

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 let pantalla = document.getElementById("pantalla");
+let tecla = document.getElementsByClassName("tecla");
+
+for (let i = 0; index < tecla.length; i++) {
+    tecla[i].addEventListener("click", () => {
+            pantalla.innerText += tecla[i].innerText;
+    })
+    
+}
