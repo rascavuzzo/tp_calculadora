6e31d35dc094ae73bbb034a317e9976b8fb84c47
Raul Scavuzzo <raul.scavuzzo@itscordoba.edu.ar>
2024-06-07 21:29:52 -0300

    modificaciones y agregado de operaciones

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
-let pantalla = document.getElementsByClassName("pantalla");
-let tecla = document.getElementsByClassName("tecla");
+let pantalla = document.getElementsById("pantalla");
+let teclas = document.getElementsByClassName("tecla");
+let operaciones = document.getElementsByClassName("operacion");
+let operacion = "";
 
-for (let i = 0; index < tecla.length; i++) {
-    tecla[i].addEventListener("click", () => {
-            pantalla.innerText += tecla[i].innerText;
+for (let i = 0; i < teclas.length; i++) {
+    teclas[i].addEventListener("click", () => {
+        pantalla.innerText += teclas[i].innerText;
+    })
+    
+}
+
+for (let i = 0; i < operaciones.length; i++) {
+    operaciones[i].addEventListener("click", () => {
+        switch (operaciones[i].innerText) {
+            case "+":
+                operacion = "suma";
+                break;
+            case "-":
+                operacion = "resta";
+                break;
+            case "*":
+                operacion = "multiplicacion";
+                break;
+            case "/":
+                operacion = "division";
+                break;        
+            default:
+                console.log("operacion no valida");
+        }
     })
     
 }
